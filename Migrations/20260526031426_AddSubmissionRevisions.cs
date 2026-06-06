1d09d3a71b7a940de73ffc5528be8279bfe5fd64
Hasim Ozudogru <hozudogru@gmail.com>
2026-06-06 06:30:26 +0300

    Fix migration order for review round index

--- Migrations/20260526031426_AddSubmissionRevisions.cs
+++ Migrations/20260526031426_AddSubmissionRevisions.cs
@@ -12,29 +12,13 @@ namespace MyDergiApp.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropIndex(
-                name: "IX_SubmissionReviewers_SubmissionId_ReviewerId",
-                table: "SubmissionReviewers");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_SubmissionReviewers_SubmissionId_ReviewerId_ReviewRound",
-                table: "SubmissionReviewers",
-                columns: new[] { "SubmissionId", "ReviewerId", "ReviewRound" },
-                unique: true);
+            
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropIndex(
-                name: "IX_SubmissionReviewers_SubmissionId_ReviewerId_ReviewRound",
-                table: "SubmissionReviewers");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_SubmissionReviewers_SubmissionId_ReviewerId",
-                table: "SubmissionReviewers",
-                columns: new[] { "SubmissionId", "ReviewerId" },
-                unique: true);
+           
         }
     }
 }
